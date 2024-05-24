f4fc35c48d5622d030d320afe07e736b042cdc69
Daniel Felps <dlfelps@gmail.com>
2024-05-24 06:18:46 -0400

    added cub and caltech datasets

---
+++ CUB200.py
@@ -0,0 +1,148 @@
+import os
+import os.path
+from pathlib import Path
+from typing import Any, Callable, List, Optional, Tuple, Union
+import re
+
+from PIL import Image
+
+from torchvision.datasets.utils import download_and_extract_archive, verify_str_arg
+from torchvision.datasets.vision import VisionDataset
+from torchvision.transforms import v2
+import torch
+
+
+
+class CUB200(VisionDataset):
+    """`CUB-200-2011 <https://www.vision.caltech.edu/datasets/cub_200_2011/>`_ Dataset.
+
+
+    Args:
+        root (str or ``pathlib.Path``): Root directory of dataset where directory
+            ``caltech101`` exists or will be saved to if download is set to True.
+        target_type (string or list, optional): Type of target to use, ``category`` or
+            ``annotation``. Can also be a list to output a tuple with all specified
+            target types.  ``category`` represents the target class, and
+            ``annotation`` is a list of points from a hand-generated outline.
+            Defaults to ``category``.
+        transform (callable, optional): A function/transform that takes in a PIL image
+            and returns a transformed version. E.g, ``transforms.RandomCrop``
+        target_transform (callable, optional): A function/transform that takes in the
+            target and transforms it.
+        download (bool, optional): If true, downloads the dataset from the internet and
+            puts it in root directory. If dataset is already downloaded, it is not
+            downloaded again.
+
+            .. warning::
+
+                To download the dataset `gdown <https://github.com/wkentaro/gdown>`_ is required.
+    """
+
+    def __init__(
+        self,
+        root: Union[str, Path],
+        transform: Optional[Callable] = None,
+        target_transform: Optional[Callable] = None,
+        is_test: bool = False,
+        download: bool = False,
+    ) -> None:
+        super().__init__(root, transform=transform, target_transform=target_transform)
+
+        if download:
+            self.download()
+
+        if not self._check_integrity():
+            raise RuntimeError("Dataset not found or corrupted. You can use download=True to download it")
+
+
+        self.root = Path(root).joinpath('CUB_200_2011')
+        self.is_test=is_test
+        self.transform = transform
+        self.target_transform = target_transform
+        self.ids = None
+        self.files = None
+        self.setup()
+
+    def setup(self):
+      ids = self.get_ids()
+      id_image_path = self.root.joinpath('images.txt')
+      with open(id_image_path, 'r') as f:
+        lines = f.readlines()
+
+      parser = image_parser_closure()
+      pairs = list(map(parser, lines))
+
+      self.ids = [k for k,_ in pairs if k in ids]
+      self.files = [v for k,v in pairs if k in ids]
+
+
+    def get_ids(self):
+
+      test_train_split_path = self.root.joinpath('train_test_split.txt')
+      with open(test_train_split_path, 'r') as f:
+        lines = f.readlines()
+
+      parser = split_parser_closure()
+      pairs = map(parser, lines)
+      training = filter(lambda x: x[1]==self.is_test, pairs)
+
+      return list(map(lambda x: x[0], training))
+
+    def __len__(self):
+        return len(self.ids)
+
+    def __getitem__(self, idx):
+        img_path = self.files[idx]
+        img = Image.open(self.files[idx])
+        target = self.ids[idx]
+
+        if self.transform is not None:
+          img = self.transform(img)
+
+        if self.target_transform is not None:
+          target = self.target_transform(target)
+
+        return img, target
+
+
+    def _check_integrity(self) -> bool:
+        # can be more robust and check hash of files
+        return os.path.exists(os.path.join(self.root, "CUB_200_2011"))
+
+
+    def download(self) -> None:
+        if self._check_integrity():
+            print("Files already downloaded and verified")
+            return
+
+        download_and_extract_archive(
+            "https://data.caltech.edu/records/65de6-vp158/files/CUB_200_2011.tgz?download=1",
+            self.root,
+            filename="CUB_200_2011.tgz",
+            md5='97eceeb196236b17998738112f37df78'
+        )
+
+
+    def extra_repr(self) -> str:
+        return "Target type: {target_type}".format(**self.__dict__)
+    
+
+# parsing functions
+def image_parser_closure():
+  parse_attr = re.compile(r'(\d+) (.+)')
+  p = Path('./CUB_200_2011/images')
+
+  def match(input):
+    result = parse_attr.match(input)
+    return (int(result.group(1)), str(p.joinpath(result.group(2))))
+
+  return match
+
+def split_parser_closure():
+  parse_two_numbers = re.compile(r'(\d+) (\d+)')
+
+  def match(input):
+    result = parse_two_numbers.match(input)
+    return (int(result.group(1)), bool(int(result.group(2))))
+
+  return match
---
+++ Caltech256.py
@@ -0,0 +1,71 @@
+import torchvision
+from torchvision.transforms import v2
+import torch
+import os
+from pathlib import Path
+from PIL import Image
+import re
+
+class CaltechNoBirds(torchvision.datasets.Caltech256):
+  """`Caltech-256 <https://data.caltech.edu/records/nyy15-4j048>`_ Dataset.
+
+
+  Args:
+      root (str or ``pathlib.Path``): Root directory of dataset where directory
+          ``caltech101`` exists or will be saved to if download is set to True.
+      target_type (string or list, optional): Type of target to use, ``category`` or
+          ``annotation``. Can also be a list to output a tuple with all specified
+          target types.  ``category`` represents the target class, and
+          ``annotation`` is a list of points from a hand-generated outline.
+          Defaults to ``category``.
+      transform (callable, optional): A function/transform that takes in a PIL image
+          and returns a transformed version. E.g, ``transforms.RandomCrop``
+      target_transform (callable, optional): A function/transform that takes in the
+          target and transforms it.
+      download (bool, optional): If true, downloads the dataset from the internet and
+          puts it in root directory. If dataset is already downloaded, it is not
+          downloaded again.
+
+          .. warning::
+
+              To download the dataset `gdown <https://github.com/wkentaro/gdown>`_ is required.
+  """
+
+  def __init__(
+        self,
+        root: str,
+        transform = None,
+        target_transform= None,
+        download: bool = False,
+    ) -> None:
+        super().__init__(root, transform=transform, target_transform=target_transform, download=download)
+        self.blacklist = [257, 114, 101, 113, 49, 60, 89, 118, 151, 152,158, 207]
+        self.files = self.__getfiles__()
+        self.class_matcher = re.compile(r'(\d+)_.+')
+
+
+  def __getfiles__(self):
+    # returns a list of non-blacklisted files
+
+    valid_files = []
+    for folder in Path(self.root).joinpath('256_ObjectCategories').iterdir():
+      if int(folder.stem) not in self.blacklist:
+        valid_files.extend(list(folder.glob('*.jpg')))
+
+    return valid_files
+  
+  def __len__(self):
+    return len(self.files)
+
+  def __getitem__(self, idx):
+    img = Image.open(self.files[idx])
+    result = self.class_matcher.match(self.files[idx].stem)
+    target = int(result.groups(1)[0])
+
+    if self.transform is not None:
+      img = self.transform(img)
+
+    if self.target_transform is not None:
+      target = self.target_transform(target)
+
+    return img, target
